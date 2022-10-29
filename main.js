5cec5c646de4151f60cfab06eabbef04f1b786a6
ananthmgk <83916278+ananthmgk@users.noreply.github.com>
2022-10-29 21:24:43 +0530

    removed drawimage func

--- main.js
+++ main.js
@@ -31,7 +31,6 @@ window.onload = function() {
     let bottomButton = document.getElementById("bottom-btn")
 
     leftButton.addEventListener("click", img1.onload = () => {
-        ctx.drawImage(img1, x, y, img1W, img1H);
         let startInterval = setInterval(leftMove, speed);
         return startInterval;
     });
